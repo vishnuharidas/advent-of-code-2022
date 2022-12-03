ad9dbdde410a08f68d5b549f4bd1e447aafe909c
Vishnu Haridas <hvishnu999@gmail.com>
2022-12-03 21:37:05 +0530

    Day 03: part 2

--- src/main/kotlin/day03/Main03.kt
+++ src/main/kotlin/day03/Main03.kt
@@ -25,8 +25,24 @@ fun main() {
 
     fun part2() {
 
+        val priorities = getFileLines("day03/input.txt")
+            .chunked(3)
+            .map {
+                it[0].toList().intersect(it[1].toList().intersect(it[2].toSet()))
+            }
+            .map {
+                when {
+                    it.first().isLowerCase() -> it.first() - 'a' + 1
+                    it.first().isUpperCase() -> it.first() - 'A' + 27
+                    else -> error("Invalid item $it")
+                }
+            }
+
+        println("Sum of priorities of badges: ${priorities.sum()}")
+
     }
 
     part1()
+    part2()
 
 }
