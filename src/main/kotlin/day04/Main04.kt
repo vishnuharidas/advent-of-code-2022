549779b6afd4c57f8e32cf7580f4279739e877ac
Vishnu Haridas <hvishnu999@gmail.com>
2022-12-06 00:28:20 +0530

    Day 04 - part 2

--- src/main/kotlin/day04/Main04.kt
+++ src/main/kotlin/day04/Main04.kt
@@ -27,8 +27,24 @@ fun main() {
 
     fun part2() {
 
+        val count = getFileLines("day04/input.txt")
+            .map {
+                val sections = it.split(",")
+                val section1 = sections[0].split("-")[0].toInt().rangeTo(sections[0].split("-")[1].toInt())
+                val section2 = sections[1].split("-")[0].toInt().rangeTo(sections[1].split("-")[1].toInt())
+                section1 to section2
+            }
+            .count {
+
+                it.first.toList().intersect(it.second.toList().toSet()).isNotEmpty()
+
+            }
+
+        println("Count of pairs with ranges overlap: $count")
+
     }
 
     part1()
+    part2()
 
 }
