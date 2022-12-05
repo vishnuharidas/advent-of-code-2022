fccde3a336f76f33eaf991fd7e032632bdca6b7c
Vishnu Haridas <hvishnu999@gmail.com>
2022-12-06 00:08:15 +0530

    Day 04 - part 1

---
+++ src/main/kotlin/day04/Main04.kt
@@ -0,0 +1,34 @@
+package day04
+
+import getFileLines
+
+fun main() {
+
+    fun part1() {
+
+        val count = getFileLines("day04/input.txt")
+            .map {
+                val sections = it.split(",")
+                val section1 = sections[0].split("-")[0].toInt().rangeTo(sections[0].split("-")[1].toInt())
+                val section2 = sections[1].split("-")[0].toInt().rangeTo(sections[1].split("-")[1].toInt())
+                section1 to section2
+            }
+            .count {
+                val section1 = it.first
+                val section2 = it.second
+
+                section1.first in section2 && section1.last in section2     // Section 1 in section 2?
+                        || section2.first in section1 && section2.last in section1 // Section 2 in section 1?
+            }
+
+        println("Count of pairs with overlaps: $count")
+
+    }
+
+    fun part2() {
+
+    }
+
+    part1()
+
+}
