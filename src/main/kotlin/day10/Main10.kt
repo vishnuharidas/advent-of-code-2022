94b1df5c3e7d9263b21f19c4c7a0bd5c489b7206
Vishnu Haridas <hvishnu999@gmail.com>
2022-12-16 01:08:00 +0530

    Day 10: part 2

--- src/main/kotlin/day10/Main10.kt
+++ src/main/kotlin/day10/Main10.kt
@@ -1,6 +1,7 @@
 package day10
 
 import getFileLines
+import kotlin.math.absoluteValue
 
 fun main() {
 
@@ -36,6 +37,43 @@ fun main() {
 
     }
 
+    fun part2() {
+
+        val lines = getFileLines("day10/input.txt")
+
+        val sprite = "###........................................".map { it }.toMutableList()
+
+        lines
+            .map {
+
+                if (it.startsWith("addx")) {
+                    listOf(0, it.split(" ")[1].toInt()) // Flatten by adding one more `noop` in front
+                } else {
+                    listOf(0) // `noop`
+                }
+
+            }
+            .flatten()
+            .foldIndexed(1) { index, acc, item ->
+
+                val pixel = (acc % 40).absoluteValue.coerceIn(1..38)
+                sprite.replaceAll { '.' }
+                sprite[pixel - 1] = '#'
+                sprite[pixel] = '#'
+                sprite[pixel + 1] = '#'
+
+                if (index % 40 == 0) println()
+
+                print(sprite[index % 40])
+
+                acc + item
+            }
+
+
+    }
+
     part1()
+
+    part2()
 
 }
