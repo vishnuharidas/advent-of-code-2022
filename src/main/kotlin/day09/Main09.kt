37bbb6e1f5769f39d669b8172c88aeb49f679518
Vishnu Haridas <hvishnu999@gmail.com>
2022-12-15 22:35:26 +0530

    Day 09: part 2

--- src/main/kotlin/day09/Main09.kt
+++ src/main/kotlin/day09/Main09.kt
@@ -109,6 +109,120 @@ fun main() {
 
     }
 
+    fun part2() {
+
+        var knots = listOf(
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0),
+            Point(0, 0)
+        )
+
+        val visits = mutableSetOf<Point>()
+
+        val lines = getFileLines("day09/input.txt")
+
+        lines
+            .map { it.split(" ") }
+            .map { it[0].first() to it[1].toInt() }
+            .forEach { line ->
+
+                val direction = line.first
+                val steps = line.second
+
+                repeat(steps) {
+
+                    when (direction) {
+                        'U' -> knots[0].up()
+                        'D' -> knots[0].down()
+                        'R' -> knots[0].right()
+                        'L' -> knots[0].left()
+                    }
+
+                    // Each knot follows the nearest knot
+                    for (i in 1..9) {
+
+                        val head = knots[i - 1]
+                        val tail = knots[i]
+
+                        if (!tail.nearby(head)) {
+
+                            when {
+
+                                // On the same Axis
+                                head.x == tail.x || head.y == tail.y -> {
+
+                                    when {
+                                        // Y axis
+                                        head.y > tail.y -> tail.up()
+                                        head.y < tail.y -> tail.down()
+
+                                        // X axis
+                                        head.x < tail.x -> tail.left()
+                                        head.x > tail.x -> tail.right()
+                                    }
+
+                                }
+
+                                // Not on axis, but on quadrants
+
+                                // Q1
+                                head.x > tail.x && head.y > tail.y -> {
+
+                                    tail.right()
+                                    tail.up()
+
+                                }
+
+                                // Q2
+                                head.x < tail.x && head.y > tail.y -> {
+
+                                    tail.left()
+                                    tail.up()
+
+                                }
+
+                                // Q3
+                                head.x < tail.x && head.y < tail.y -> {
+
+                                    tail.left()
+                                    tail.down()
+
+                                }
+
+                                // Q4
+                                head.x > tail.x && head.y < tail.y -> {
+
+                                    tail.right()
+                                    tail.down()
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                    // Add the tail to the set
+                    visits.add(knots.last().copy())
+
+                }
+
+            }
+
+        println("Tail visits at least once (with 10 knots): ${visits.size}")
+
+    }
+
     part1()
+
+    part2()
 
 }
