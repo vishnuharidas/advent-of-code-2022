3bf4b31dd674532278c12d069fb2617cdd62a5b1
Vishnu Haridas <hvishnu999@gmail.com>
2022-12-16 20:23:30 +0530

    Day 11: part 2

--- src/main/kotlin/day11/Main11.kt
+++ src/main/kotlin/day11/Main11.kt
@@ -6,6 +6,7 @@ internal data class Monkey(
     val list: MutableList<Long>,
     val operation: (Long) -> Long,
     val test: (Long) -> Boolean,
+    val divisibleBy: Long,
     val trueMonkey: Int,
     val falseMonkey: Int,
     var inspectionCount: Int = 0,
@@ -27,15 +28,15 @@ fun main() {
 
         val falseMonkey = lines[5].split("throw to monkey")[1].trim().toInt()
 
-        val (op1, operation, op2) = lines[2]
+        val (operation, op2) = lines[2]
             .split(" ")
             .map { it.trim() }
-            .takeLast(3)
+            .takeLast(2)
 
         val op = when (operation) {
 
-            "+" -> { i: Long -> (if (op1 == "old") i else op1.toLong()) + (if (op2 == "old") i else op2.toLong()) }
-            "*" -> { i: Long -> (if (op1 == "old") i else op1.toLong()) * (if (op2 == "old") i else op2.toLong()) }
+            "+" -> { i: Long -> i + (if (op2 == "old") i else op2.toLong()) }
+            "*" -> { i: Long -> i * (if (op2 == "old") i else op2.toLong()) }
 
             else -> error("Invalid operation")
 
@@ -45,6 +46,7 @@ fun main() {
             list = startingItems.toMutableList(),
             operation = op,
             test = { it % divisibleBy == 0L },
+            divisibleBy = divisibleBy,
             trueMonkey = trueMonkey,
             falseMonkey = falseMonkey
         )
@@ -87,12 +89,53 @@ fun main() {
         }
 
         val (i, j) = monkeys.map { it.inspectionCount }
-            .sorted().also { println(it) }
+            .sorted()
             .takeLast(2)
 
         println("Level of monkey business after 20 rounds: ${i * j}")
 
     }
 
+    fun part2() {
+
+        val monkeys = getMonkeys("day11/input.txt")
+
+        val newDivisor = monkeys.map { it.divisibleBy }.reduce { acc, l -> acc * l }
+
+        repeat(10_000) { r ->
+
+            monkeys.forEach { monkey ->
+
+                if (monkey.list.isEmpty()) return@forEach // Empty monkey
+
+                monkey.inspectionCount += monkey.list.size
+
+                monkey.list
+                    .map { monkey.operation(it) % newDivisor }
+                    .forEach { item ->
+
+                        // Throw to the other monkey
+                        monkeys[
+                            if (monkey.test(item)) monkey.trueMonkey else monkey.falseMonkey
+                        ].list.add(item)
+                    }
+
+                monkey.list.clear()
+
+            }
+
+        }
+
+        val (i, j) = monkeys.map { it.inspectionCount }
+            .sorted()
+            .map { it.toLong() }
+            .takeLast(2)
+
+        println("Level of monkey business after 10000 rounds (w/o division by 3, and with new logic): ${i * j}")
+
+    }
+
     part1()
+
+    part2()
 }
